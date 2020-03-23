a81d8dfffbf0e933db8aa93ed596cd6adc2ccc15
lukas <lm@netzlabor.de>
2020-03-23 16:07:25 +0100

    fix data not updating in periodical info updates

--- covid_parser.py
+++ covid_parser.py
@@ -1,6 +1,9 @@
 import urllib.request, csv 
 import datetime
 
+URL_CONFIRMED = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+URL_DEATHS = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv"
+
 def parse_csv(url):
     csv_file = download_csv(url)
 
@@ -39,31 +42,35 @@ def add_rows(row1, row2):
         return [figure + row2[i] for i, figure in enumerate(row1)]
 
 def download_csv(url):
-    with  urllib.request.urlopen(url) as req:
+    with urllib.request.urlopen(url) as req:
         csv_file = req.read().decode().split('\n')
     return csv_file
 
 def get_data_by_type(type):
     if type == 'deaths':
+        dates_d, deaths = parse_csv(URL_DEATHS)
         return deaths
     if type == 'confirmed':
+        dates_c, confirmed = parse_csv(URL_CONFIRMED)
         return confirmed
     else:
         print('no data found with type ', type)
         return None
 
 def get_dates_by_type(type):
-    update_data()
     if type == 'deaths':
+        dates_d, deaths = parse_csv(URL_DEATHS)
+        dates_deaths = [datetime.datetime.strptime(date, "%m/%d/%y") for date in dates_d]
         return [date.strftime('%d.%m.') for date in dates_deaths]
     if type == 'confirmed':
+        dates_c, confirmed = parse_csv(URL_CONFIRMED)
+        dates_confirmed = [datetime.datetime.strptime(date, "%m/%d/%y") for date in dates_c]
         return [date.strftime('%d.%m.') for date in dates_confirmed]
     else:
         print('no data found with type ', type)
         return None
 
 def today(data_type, country=None, offset=-1):
-    update_data()
     data = get_data_by_type(data_type)  
     if country:
         return data[country][offset]
@@ -71,28 +78,11 @@ def today(data_type, country=None, offset=-1):
         return sum([country[offset] for country in data.values()])    
 
 def increase_daily(data_type, country=None):
-    update_data()
     t = today(data_type, country)
     yesterday = today(data_type, country, -2)
     if yesterday == 0: return '~'
     return round( (t * 100 / yesterday) - 100 )
 
 def countries():
+    dates_d, deaths = parse_csv(URL_DEATHS)
     return deaths.keys()
-
-def update_data():
-    dates_d, deaths = parse_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv")
-    dates_deaths = [datetime.datetime.strptime(date, "%m/%d/%y") for date in dates_d]
-
-    dates_c, confirmed = parse_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
-    dates_confirmed = [datetime.datetime.strptime(date, "%m/%d/%y") for date in dates_c]
-
-# Deaths
-dates_d, deaths = parse_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv")
-
-dates_deaths = [datetime.datetime.strptime(date, "%m/%d/%y") for date in dates_d]
-
-# Confirmed
-dates_c, confirmed = parse_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
-
-dates_confirmed = [datetime.datetime.strptime(date, "%m/%d/%y") for date in dates_c]
--- discord_cov_bot_19.py
+++ discord_cov_bot_19.py
@@ -15,15 +15,15 @@ def info_message_std(country='Germany'):
     ) + message_country(country) + (
         f"🌍 Welt:\n"
         f"  → **Heute** ({get_dates_by_type('deaths')[-1]}):\n"
-        f"    😷 {today('confirmed')} bestätigte Infizierte (+{increase_daily('confirmed')}%); "
-        f"  💀 {today('deaths')} Todesfälle (+{increase_daily('confirmed')}%)\n\n"
+        f"    😷 {today('confirmed')} bestätigte Infizierte (+{increase_daily('confirmed')}% im vgl. zum Vortag); "
+        f"  💀 {today('deaths')} Todesfälle (+{increase_daily('confirmed')}% im vgl. zum Vortag)\n\n"
     )
 
 def message_country(country):
     return (
         f"  → **Heute** ({get_dates_by_type('deaths')[-1]}):\n"
-        f"    😷 {today('confirmed', country)} bestätigte Infizierte (+{increase_daily('confirmed', country)}%)"
-        f"  💀 {today('deaths', country)} Todesfälle (+{increase_daily('deaths', country)}%)\n"
+        f"    😷 {today('confirmed', country)} bestätigte Infizierte (+{increase_daily('confirmed', country)}% im vgl. zum Vortag)"
+        f"  💀 {today('deaths', country)} Todesfälle (+{increase_daily('deaths', country)}% im vgl. zum Vortag)\n"
     )
 
 print(info_message_std())
@@ -67,11 +67,14 @@ async def nine_nine(ctx, arg):
     if arg not in countries():
         await ctx.send("Land nicht gefunden.")
     else:
-        await ctx.send(f"{arg}:\n" + message_country(country=arg))
+        msg = message_country(country=arg)
+        await ctx.send(f"{arg}:\n" + msg)
 
 @loop(hours=24.0)
 async def info_loop():
-    await covid19_channel.send(info_message_std())
+    msg = info_message_std()
+    print(msg)
+    await covid19_channel.send(msg)
 
 @info_loop.before_loop
 async def info_before():
@@ -79,10 +82,6 @@ async def info_before():
     await bot.wait_until_ready()
     guild = discord.utils.get(bot.guilds, name=GUILD)
     covid19_channel = get_channel(guild, CHANNEL)
-
-
-
-
 
 info_loop.start()
 bot.run(TOKEN)
